7299987a9f4c3f56b70e92ff54e9cd9d38a7325c
Matthew Daly <matthewbdaly@gmail.com>
2017-02-07 21:07:36 +0000

    Initial commit

---
+++ tests/EtagTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use Mockery as m;
+use Illuminate\Http\Request;
+use Matthewbdaly\ETagMiddleware\ETag;
+use PHPUnit_Framework_TestCase;
+
+class ETagTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test new request not cached
+     *
+     * @return void
+     */
+    public function testModified()
+    {
+        // Create mock response
+        $response = m::mock('Illuminate\Http\Response')->shouldReceive('getContent')->once()->andReturn('blah')->getMock();
+        $response->shouldReceive('setEtag')->with(md5('blah'));
+
+        // Create request
+        $request = Request::create('http://example.com/admin', 'GET');
+
+        // Pass it to the middleware
+        $middleware = new ETag();
+        $middlewareResponse = $middleware->handle($request, function () use ($response) { 
+            return $response;
+        });
+    }
+
+    /**
+     * Test repeated request not modified
+     *
+     * @return void
+     */
+    public function testNotModified()
+    {
+        // Create mock response
+        $response = m::mock('Illuminate\Http\Response')->shouldReceive('getContent')->once()->andReturn('blah')->getMock();
+        $response->shouldReceive('setEtag')->with(md5('blah'));
+        $response->shouldReceive('setNotModified');
+
+        // Create request
+        $request = Request::create('http://example.com/admin', 'GET', [], [], [], [
+            'ETag' => md5('blah')
+        ]);
+
+        // Pass it to the middleware
+        $middleware = new ETag();
+        $middlewareResponse = $middleware->handle($request, function () use ($response) { 
+            return $response;
+        });
+    }
+}
